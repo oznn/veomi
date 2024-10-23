a1ccfdad4f80acf209661a45896ad50155105dcc
unknown <ozzen0001@gmail.com>
2024-10-23 09:43:35 +0100

    v0.5.0

--- src/renderer/extensions.ts
+++ src/renderer/extensions.ts
@@ -2,5 +2,5 @@ export default {
   hianime: { name: 'Hianime', lang: 'en' },
   mangadex: { name: 'Mangadex', lang: 'en' },
   mangasee: { name: 'Mangasee', lang: 'en' },
-  myflixer: { name: 'MyFlixer', lang: 'en' },
+  // myflixer: { name: 'MyFlixer', lang: 'en' },
 } as { [k: string]: { name: string } };
